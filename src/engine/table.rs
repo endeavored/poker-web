4e32c36536583af764d9f2df92d279875ab6fa53
endeavored <christray100@gmail.com>
2026-02-08 04:07:48 -0500

    Add pot, user placement, active

--- src/engine/table.rs
+++ src/engine/table.rs
@@ -1,8 +1,17 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 use crate::engine::player;
 
+#[derive(Hash)]
+pub struct TablePlayer {
+    active: bool,
+    stack: u32,
+    position: u8,
+}
+
 pub struct Table {
-    player_list: HashSet<player::PlayerId>,
+    dealer_location: u8,
+    player_list: HashMap<player::PlayerId, TablePlayer>,
+    pot: u32,
     max_table_size: u8,
 }
