fdc3e73c86f826d92e70f7eae2b609bd8faea58a
endeavored <christray100@gmail.com>
2026-02-09 00:28:22 -0500

    Add bet to table

--- src/engine/bet.rs
+++ src/engine/bet.rs
@@ -6,5 +6,6 @@ use crate::engine::player::PlayerId;
 pub struct Bet {
     pub player_id: PlayerId,
     pub amount: u32,
+    pub table_round: u8,
     timestamp: DateTime<Utc>,
 }
--- src/engine/table.rs
+++ src/engine/table.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::vec;
 
+use crate::engine::bet;
 use crate::engine::hand;
 use crate::engine::player;
 
@@ -13,9 +14,10 @@ pub struct HoldEmTablePlayer {
 }
 
 pub struct HoldEmTable {
-    bet_history: Vec<u32>,
+    bet_history: Vec<bet::Bet>,
     dealer_location: u8,
     player_list: HashMap<player::PlayerId, HoldEmTablePlayer>,
     pot: u32,
+    round: u8,
     max_table_size: u8,
 }
