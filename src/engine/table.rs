9162efc286a23149382d6422dcf0a62699da9288
endeavored <christray100@gmail.com>
2026-02-09 01:42:38 -0500

    Add place_bet

--- src/engine/table.rs
+++ src/engine/table.rs
@@ -10,17 +10,33 @@ use crate::engine::player;
 pub struct HoldEmTablePlayer {
     active: bool,
     hand: hand::HoldEmHand,
-    stack: u32,
+    pub stack: u32,
     position: u8,
 }
 
-pub struct HoldEmTable {
+pub struct HoldEmTable<'a> {
     active_players: LinkedList<player::Player>,
     bet_history: Vec<bet::Bet>,
     board: Option<[card::Card; 5]>,
     dealer_location: u8,
     max_table_size: u8,
-    player_list: HashMap<player::PlayerId, HoldEmTablePlayer>,
+    player_list: HashMap<player::PlayerId, &'a mut HoldEmTablePlayer>,
     pot: u32,
     round: u8,
+}
+
+impl<'a> HoldEmTable<'a> {
+    fn place_bet(&mut self, player_id: player::PlayerId, amount: u32) {
+        if !self.player_list.contains_key(&player_id) {
+            return;
+        }
+        let user = self.player_list.get_mut(&player_id).unwrap();
+        if user.stack < amount {
+            return;
+        }
+        user.stack -= amount;
+        self.pot += amount;
+        let user_bet = bet::Bet::new(player_id, amount, self.round);
+        self.bet_history.push(user_bet);
+    }
 }
