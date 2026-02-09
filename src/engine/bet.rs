51e58f946b0ce3350277af8b193272f8be282e70
endeavored <christray100@gmail.com>
2026-02-09 00:33:08 -0500

    Add bet

--- src/engine/bet.rs
+++ src/engine/bet.rs
@@ -9,3 +9,14 @@ pub struct Bet {
     pub table_round: u8,
     timestamp: DateTime<Utc>,
 }
+
+impl Bet {
+    pub fn new(player_id: PlayerId, amount: u32) -> Self {
+        Self {
+            player_id: player_id,
+            amount: amount,
+            table_round: 0,
+            timestamp: chrono::offset::Utc::now(),
+        }
+    }
+}
